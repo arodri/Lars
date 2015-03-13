19be196b9ce9c7d700f59d5d634eebb569409b6f
Aaron Rodriguez <vknight7@gmail.com>
2015-03-13 18:18:54 -0400

    Simple util for cleaning up keys before dumping. Or whatever.

---
+++ lars/mappers/remove.py
@@ -0,0 +1,14 @@
+from mapper import Mapper
+from datetime import datetime as dt
+class RemoveField(Mapper):
+
+	def loadConfigJSON(self,config):
+		self.name = config["name"]
+		self.removeFields = config["removeFields"]
+
+	def process(self,record):
+		for field in self.removeFields:
+			if field in record:
+				del(record[field])
+		return record
+
