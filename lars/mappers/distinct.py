5ae82462438f60887bd7e2829a3ca8c22b37c132
Aaron Rodriguez <arodriguez@xor.exchange>
2015-04-21 12:32:34 -0400

    Removed deepcopy

--- lars/mappers/distinct.py
+++ lars/mappers/distinct.py
@@ -1,21 +1,17 @@
 from mapper import Mapper
-import copy
 
 class MapDistinct(Mapper):
 	def loadConfigJSON(self,config):
 		self.name = config["name"]
 		self.distinct_fields = config["distinct_fields"]
 		self.record_lists = config["record_lists"]
-		
-		self.provides = []
-		self.mapped = {}
-		for rl in self.record_lists:
-			for f in self.distinct_fields:
-				self.provides.append("%s_DISTCNT_%s" % (rl, f))
-				self.mapped[(rl,f)] = {}
+
+		self.dict_factory = make_distinct_dict_factory(self.record_lists, self.distinct_fields)
+		self.provides = [ "%s_DISTCNT_%s" % (rl, f) for (rl,f) in self.dict_factory().keys() ]
+
 	def process(self,record):
 		# dict to hold the histogram
-		mapped = copy.deepcopy(self.mapped)
+		mapped = self.dict_factory()
 		for rl in self.record_lists:
 			for rl_rec in record[rl]:
 				for f in self.distinct_fields:
@@ -33,16 +29,11 @@ class DistinctCount(Mapper):
 		self.record_lists = config["record_lists"]
 		self.exclude_field_values = config["exclude_field_values"]
 		
-		self.provides = []
-		self.uniq_vals = {}
-		for rl in self.record_list:
-			for f in self.distinct_fields:
-				self.provides.append("%s_uniq_%s" % (rl, f))
-				self.uniq_vals[(rl,f)] = set()
+		self.dict_factory = make_distinct_dict_factory(self.record_lists, self.distinct_fields)
+		self.provides = [ "%s_uniq_%s" % (rl, f) for (rl,f) in self.dict_factory().keys() ]
 
-		
 	def process(self,record):
-		unique_vals = copy.deepcopy(self.uniq_vals)
+		unique_vals = self.dict_factory()
 		exclude_values = set()
 		for ef in self.exclude_field_values:
 			exclude_values.add(record[ef])
@@ -52,9 +43,19 @@ class DistinctCount(Mapper):
 				for f in self.distinct_fields:
 					v = rl_rec[rl][f]
 					if v not in exclude_values:
-						unique_vals[(rl,f)].add(v)
+						unique_vals[(rl,f)][v] = 1
 
 		for rl in self.record_list:
 			for f in self.distinct_fields:
-				record["%s_uniq_%s" % (rl, f)] = len(unique_vals[(rl,f)])
+				record["%s_uniq_%s" % (rl, f)] = len(unique_vals[(rl,f)].keys())
 		return record
+
+def make_distinct_dict_factory(record_lists, distinct_fields):
+	def factory():
+		mapped = {}
+		for rl in record_lists:
+			for f in distinct_fields:
+				mapped[(rl,f)] = {}
+		return mapped
+	return factory
+
