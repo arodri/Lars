ae7e7bac45dd5fdfd31faeacf594df4de77c0873
Greg Bonin <gbonin@xor.exchange>
2015-07-10 16:09:40 -0400

    initial checkin of tool to give you mapping between mapperNames to the outputs they provide. Relies on .provides

---
+++ scripts/workflowProvides.py
@@ -0,0 +1,20 @@
+from lars.workflow import Workflow
+import json
+import sys
+
+def getProvides(config):
+	wf = Workflow()
+	wf.buildJSON(config)
+	res = {}
+	for mapper,_ in wf.mappers:
+		print mapper
+		res[mapper.name] = mapper.provides
+	return res
+	
+
+
+res =  getProvides(json.load(sys.stdin))
+print json.dumps(res,indent=4, separators=(",", ":"))
+
+
+
