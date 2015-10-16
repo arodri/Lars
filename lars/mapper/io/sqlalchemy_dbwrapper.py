7f7dcde23992207bca204b1c8a7716da14873e11
Greg Bonin <gbonin@xor.exchange>
2015-10-16 15:26:28 -0500

    added keep-alive query

--- lars/mapper/io/sqlalchemy_dbwrapper.py
+++ lars/mapper/io/sqlalchemy_dbwrapper.py
@@ -2,6 +2,24 @@ import sqlalchemy
 import time
 import logging
 from .dbwrapper import DBWrapper
+import threading
+from threading import Timer
+import random 
+
+class PeriodicTask(object):
+    def __init__(self,interval,callback, daemon=True, jitter=0, **kwargs):
+        self.interval = interval
+        self.callback = callback
+        self.daemon = daemon
+        self.kwargs = kwargs
+        self.jitter = interval*jitter
+
+    def run(self):
+            self.callback(**self.kwargs)
+            interval = self.interval+(self.jitter*random.uniform(-1,1))
+            t = Timer(interval,self.run)
+            t.daemon = self.daemon
+            t.start()
 
 class SQLAlchemyDBWrapper(DBWrapper):
 	def __init__(self, config):
@@ -16,6 +34,17 @@ class SQLAlchemyDBWrapper(DBWrapper):
 			logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
                 self.max_attempts = config.get("max_attempts",3)
                 self.initial_delay = config.get("initial_delay",.05)
+                ping = config.get("ping_delay",1)
+                #check to see if connection params are good
+                t = PeriodicTask(ping, self.arod_query)
+                t.run()
+                try:
+                    self.arod_query()
+                except sqlalchemy.exc.OperationalError:
+                    raise Exception("Failed to connect using url %s" % self.engine_url)
+
+        def arod_query(self):
+            self._cnx_pool.execute("SELECT 'I AM A DUMMY'")
 
         def retry_execs(self,query,params):
             attempts = 1
