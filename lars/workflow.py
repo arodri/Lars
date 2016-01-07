7d7e05e3f4dcf63170251f9a75426b585cac5fcc
Greg Bonin <gbonin@xor.exchange>
2016-01-07 10:16:49 -0600

    added ability to include environement variables in lars. Uses yaml syntax

--- lars/workflow.py
+++ lars/workflow.py
@@ -4,9 +4,11 @@ import sys
 import json
 import logging
 import sys
+import os
 import time
 import argparse
 import socket
+import re
 
 from feeder import FileReader
 from lars import mapper 
@@ -16,7 +18,10 @@ from lars import log
 #todo- make class vars instead of strings
 #TODO: Name the workflows so that you can auto assign record_ids in a multi-threaded environment
 
+ENV_RE = re.compile("<%= ENV\[\'.+?\'\] %>")
+
 class Workflow(object):
+
 	
 	def __init__(self):
 		logger = logging.getLogger('lars.workflow')
@@ -30,6 +35,22 @@ class Workflow(object):
 			"applicationname":"lars"
 			}
 		self.logger = log.LarsLoggerAdapter(logger,self.context)
+        
+        def buildJSON_env(self,wf_str,instanceID=None):
+            replacements = ENV_RE.findall(wf_str)
+            bads = []
+            for replace in replacements:
+                env_var = replace.split("ENV['")[1].split("']")[0]
+                env_val =  os.environ.get(env_var)
+                if env_val:
+                    wf_str = wf_str.replace(replace,env_val)
+                else:
+                    bads.append(env_var)
+            if len(bads) != 0:
+                raise KeyError("The following environment variables do not exist %s" % bads)
+            else:
+                self.buildJSON(json.loads(wf_str),instanceID)
+                    
 
 	def buildJSON(self, config,instanceID=None):
 		self.context['workflow'] = instanceID
@@ -48,7 +69,7 @@ class Workflow(object):
 					self.mappers.append(mapperTup)
 					self.mapperDict[thisMap.name] = mapperTup
 					self.logger.info("Parsed %s" % thisMap.name)
-			for outConfig in wf['outputters']:
+			for outConfig in wf.get('outputters',[]):
 				#default outputter is delimited outputter
 				thisOutClass = DelimitedOutputter
 				if outConfig.has_key("class"):
@@ -156,7 +177,8 @@ if __name__ == "__main__":
 
 	wf = Workflow()
 	with args.workflow[0] as wfFH:
-		wf.buildJSON(json.load(wfFH))
+                workflow_str = wfFH.read()
+		wf.buildJSON_env(workflow_str)
 
 	with args.input_file[0] as recordFH:
 		reader = FileReader(recordFH, args.d)
